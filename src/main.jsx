d38847a1415f6e2db96af8f3be1b887097e5e75d
FahimX <sakibfahim270@gmail.com>
2025-05-15 04:21:35 +0800

    basic-layout-setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./LayOuts/MainLayout";
+import Home from "./Components/Home";
+import AddCoffee from "./Components/AddCoffee";
+import UpdateCoffee from "./Components/UpdateCoffee";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/addcoffee",
+        Component: AddCoffee,
+      },
+      {
+        path: "/updateCoffee",
+        Component: UpdateCoffee,
+      },
+    ],
   },
 ]);
 
